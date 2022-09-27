19e39c1e94367f6ef8ca453bba516014f491a5c3
yugo uchiida <y.uchiida@nirneege.com>
2022-09-28 00:55:59 +0900

    add firebase settings

---
+++ src/firebase.ts
@@ -0,0 +1,25 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+/* .env で設定したfirebase の設定を読み込む */
+const firebaseConfig = {
+	apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+	databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+	appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+/* 設定したコンフィグのオブジェクトを読み込んで、firebaseを初期化する */
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore();
+export const storage = getStorage();
+export const auth = getAuth();
+export const provider = new GoogleAuthProvider();
+
+export default app;
